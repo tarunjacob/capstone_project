678b94a2c4acae0a4c38d82989289cd39f34d460
Tarun Jacob <tarunjacob42@gmail.com>
2021-10-13 14:41:07 +1100

    carousel added; check img load error

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Typography, Divider, Paper } from '@mui/material';
 import axios from 'axios';
-
+import Carousel from 'react-material-ui-carousel';
 const Dashboard = () => {
     const first_name = localStorage.getItem('name');
     const [name, setName] = React.useState(first_name);
-    return (
-        <Typography>
-            {name 
-            ?<div>Welcome to your dashboard {name} </div>
-            :<div>Please Log in or sign up for the best experience!</div>
+    const [products, setProducts] = React.useState([]);
+    const getProducts = async () => {
+        try {
+            const res = await axios.get('http://localhost:8000/products/view');
+            setProducts(res.data);
+            console.log(res.data);
         }
-        </Typography>
+        catch (err) {
+            console.log(err);
+        }
+    }
+    React.useEffect(() => {
+        getProducts();
+    }, []);
+    return (
+        <div>
+            <Typography variant='h6'>
+                {name
+                    ? <div>Welcome to your dashboard {name} </div>
+                    : <div>Please Log in or sign up for the best experience!</div>
+                }
+            </Typography>
+            <Divider />
+            <Carousel>
+                {products.map((product, i) => {
+                    return (
+                        <div>
+                                {/* <img height='400' src={product.product_image} alt={product.product_image} /> */}
+
+                            <Paper>
+                                {product.product_name}
+                                <img height='400' src={product.product_image} alt={product.product_name} />
+                            </Paper>
+                        </div>
+                    )
+                })}
+            </Carousel>
+        </div>
     )
 }
 
